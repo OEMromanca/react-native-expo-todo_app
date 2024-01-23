681a7b83524b6854b6dea57e99e23d791c36c80a
romanca <r.jamrisko@gmail.com>
2024-01-23 21:44:40 +0100

    minor changes

--- src/redux/actions/actionsOperations/snackBarActionsOperations.tsx
+++ src/redux/actions/actionsOperations/snackBarActionsOperations.tsx
@@ -1,16 +1,10 @@
-import { FILTER_SNACKBAR_MAP } from "../../../utils/filterMaps";
 import { AppDispatch, TSnackbarKey } from "../../../types/types";
 import { snackBarActions } from "../snackBarActions";
 
 export const updateSnackBar = (key: TSnackbarKey) => {
   return (dispatch: AppDispatch) => {
-    if (!FILTER_SNACKBAR_MAP[key]) {
-      console.warn(`Invalid key: ${key} updateSnackBar actions`);
-      return;
-    } else {
-      dispatch(snackBarActions.updateSnackBarContent({ key }));
-      dispatch(snackBarActions.showSnackBarTrue({ showSnackBar: true }));
-    }
+    dispatch(snackBarActions.updateSnackBarContent({ key }));
+    dispatch(snackBarActions.showSnackBarTrue({ showSnackBar: true }));
   };
 };
 
